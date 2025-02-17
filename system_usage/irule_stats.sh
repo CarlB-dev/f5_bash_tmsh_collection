25b3202f677eeac9e38e20785201f9f72cc9ad08
Carl B <44070777+CarlB-dev@users.noreply.github.com>
2025-02-17 12:53:42 -0500

    Corrected issue with irulestat.sh

--- system_usage/irule_stats.sh
+++ system_usage/irule_stats.sh
@@ -4,4 +4,4 @@
 tmctl rule_stat -s name,event_type,avg_cycles,total_executions,max_cycles,min_cycles,failures,aborts -O -K total_executions,avg_cycles -L 10 -w 300  | awk 'NR <= 2 || $4 != 0'
 
 # All irules
-tmctl rule_stat -s name,event_type,avg_cycles,total_executions,max_cycles,min_cycles,failures,aborts -O -K total_executions,avg_cycles -L 10 -w 300  | awk 'NR <= 2 || $4 != 0'
+tmctl rule_stat -s name,event_type,avg_cycles,total_executions,max_cycles,min_cycles,failures,aborts -O -K total_executions,avg_cycles -w 300  | awk 'NR <= 2 || $4 != 0'
