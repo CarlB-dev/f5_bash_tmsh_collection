6edc2dc96839e91867e7a4d9cae57d8fec547c38
Carl B <44070777+CarlB-dev@users.noreply.github.com>
2025-02-24 10:52:15 -0500

    Beginning Unused Object script

---
+++ system_usage/unused_objects.sh
@@ -0,0 +1,9 @@
+# In development
+# We will take a look at objects that have not seen traffic, virtual servers, pools, pool members, etc
+# Assumptions - that the platform has been running and collecting statistics for ~30 days and none of the objects are part of a hot standby, DR/BC configuration set.
+
+# Virtual Servers that have had no inbound traffic
+tmsh -q -c "cd /; list ltm virtual recursive one-line" | awk '{print "/"$3}' | while read vs; do if [ "$(tmsh show ltm virtual $vs field-fmt raw | grep 'clientside.bits-in' | awk '{print $2}')" -eq 0 ]; then echo $vs; fi; done
+
+# Pools - we will look at them from a few angles, available members, pick count, etc
+tmsh -q -c "cd /; list ltm pool recursive one-line" | awk '{print "/"$3}' | while read pool; do if [ "$(tmsh show ltm pool $pool field-fmt raw | grep 'clientside.bits-in' | awk '{print $2}')" -eq 0 ]; then echo $pool; fi; done
