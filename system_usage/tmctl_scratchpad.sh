6a92b329ef37fa7888e2ee8b94cc54e0bf608a6b
Carl B <44070777+CarlB-dev@users.noreply.github.com>
2025-03-12 09:40:26 -0400

    More Tmctl exploration

--- system_usage/tmctl_scratchpad.sh
+++ system_usage/tmctl_scratchpad.sh
@@ -1,14 +1,24 @@
 # exploring Telemetry Statistic Control Tool use cases
 # Top 15 memory consumers at a specific timestamp
+# This uses the historical data and will take time to process
 tmctl -lD /shared/tmstat/snapshots/ proc_pid_stat -s rss,time,proc_name 2>/dev/null | grep "2025-03-10T10:00:00" | sort -nrk1 | head -n 15
 # Looking for memory growth over time of devmgmtd
+# This uses the historical data and will take time to process
 tmctl -lD /shared/tmstat/snapshots/ proc_pid_stat -s rss,time,proc_name proc_name=devmgmtd 2>/dev/null
 
+# Historical Pool pick
+# issue - does not show timestamp for when the data was collected
+tmctl -lD /shared/tmstat/snapshots/ pool_stat -s pool_name,pick_tot 2>/dev/null
+# To specify the pool name, either wrap in single quotes or escape the special characters
+tmctl -lD /shared/tmstat/snapshots/ pool_stat -s pool_name,pick_tot pool_name='/Common/app-3' 2>/dev/null 
+
 # Looking for CPU usage over time of monitoring daemon bigd
+# This uses the historical data and will take time to process
 tmctl -lD /shared/tmstat/snapshots/ proc_pid_stat -s cpu_usage_recent,system_usage_recent,time,proc_name,pid proc_name=bigd 2>/dev/null
 
 # looking for uneven distribution of traffic across tmm instances (4tmm example)
 # src - https://my.f5.com/manage/s/article/K91433389
+cd /var/tmstat/blade
 # Current
 paste <(tmctl -f tmm0 virtual_server_stat --select=name,clientside.cur_conns --sortby=name) <(tmctl -f tmm1 virtual_server_stat --select=clientside.cur_conns --sortby=name) <(tmctl -f tmm2 virtual_server_stat --select=clientside.cur_conns --sortby=name) <(tmctl -f tmm3 virtual_server_stat --select=clientside.cur_conns --sortby=name)
 # Historical since reboot
